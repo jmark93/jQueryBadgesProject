32ad3fa698e2f2e56e4a24ec80a0bb7eec6c8b32
jmark93 <jmark93@me.com>
2016-12-18 20:01:43 -0600

    gettingStarted
    The first change.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,17 @@
 $(function() {
+	
+$.ajax({
+	url: 'https://www.codeschool.com/users/jmark93/.json',
+  	dataType: 'jsonp',
+  	success: function(response) {
+		$.each(response, function(i) {
+			var favorite = $('.favorite-' + index);
+			favorite.find('p').html(city.name);
+			favorite.find('img')
+				.attr('src', city.image);
+	});
 
-  // your code will go here
+};
+	//https://www.codeschool.com/projects/use-jquery-to-fetch-and-show-code-school-badges-using-ajax/build#answer-video
+    
 
-});
